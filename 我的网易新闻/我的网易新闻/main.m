4fc4b9ceff83f6a1322af8ed33f372ecabb2713f
GMarcello <917187502@qq.com>
2016-06-14 18:57:32 +0800

    项目初始化

---
+++ 我的网易新闻/我的网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  我的网易新闻
+//
+//  Created by Marcello on 16/6/14.
+//  Copyright © 2016年 Marcello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
